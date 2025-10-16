39115f8e11412d78d912ae45c1afbd757644b628
thedax <thedax@users.noreply.github.com>
2025-10-16 08:38:44 -0400

    Update for 7.35h1

--- src/structs/zone_ipc_def.rs
+++ src/structs/zone_ipc_def.rs
@@ -3,25 +3,25 @@ use num_enum::{IntoPrimitive, TryFromPrimitive};
 #[derive(Debug, Eq, PartialEq, IntoPrimitive, TryFromPrimitive)]
 #[repr(u16)]
 pub enum ServerZoneIpcType {
-  Ping = 0x065, // 7.3h1
-  Init = 0x067, // 7.3h1
-  InitZone = 0x36C, // 7.3h1
+  Ping = 0x078, // 7.35h1
+  Init = 0x30E, // 7.35h1
+  InitZone = 0x3A3, // 7.35h1
 
-  PlayerStats = 0x0E6,   // 7.3h1
-  PlayerSetup = 0x359,   // 7.3h1
-  PlayerClassInfo = 0x22D, // 7.3h1
-  PlayerSpawn = 0x37A,   // 7.3h1
+  PlayerStats = 0x3AD,   // 7.35h1
+  PlayerSetup = 0x26F,   // 7.35h1
+  PlayerClassInfo = 0x2CC, // 7.35h1
+  PlayerSpawn = 0x0E3,   // 7.35h1
 
-  ActorControlSelf = 0x1F4, // 7.3h1
+  ActorControlSelf = 0x3BD, // 7.35h1
 }
 
 #[derive(Debug, Eq, PartialEq, IntoPrimitive, TryFromPrimitive)]
 #[repr(u16)]
 pub enum ClientZoneIpcType {
   None,
-  PingHandler = 0x0F1, // 7.3h1
-  InitHandler = 0x152, // 7.3h1
-  FinishLoadingHandler = 0x336, // 7.3h1
+  PingHandler = 0x3E0, // 7.35h1
+  InitHandler = 0x1AB, // 7.35h1
+  FinishLoadingHandler = 0x197, // 7.35h1
 }
 
 #[derive(Debug, Eq, PartialEq, IntoPrimitive, TryFromPrimitive)]
